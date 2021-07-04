4c589cfdf8d7bb927e8511605b7188ff227600eb
KennetAvila28 <83607313+KennetAvila28@users.noreply.github.com>
2021-07-04 14:41:16 -0500

    feat: about page

--- Jaguarete/urls.py
+++ Jaguarete/urls.py
@@ -2,7 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 
+import eCommerce.views
 
 urlpatterns = [
+    path('', eCommerce.views.index),
+    path('about', eCommerce.views.about),
     path('admin/', admin.site.urls),
 ]
--- eCommerce/views.py
+++ eCommerce/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def index(request):
+    url = "https://jsonplaceholder.typicode.com/users"
+    ur2 = "https://jsonplaceholder.typicode.com/photos"
+    r = requests.get(url)
+    r2 = requests.get(ur2)
+    json = r.json()
+    json2 = r2.json()
+    return render(request, 'Index.html', {
+        'title': "Jaguarete - Ecommerce",
+        'products': json,
+        'photos': json2
+    })
+def about(request):
+    return render(request,'About.html', {
+        'title': "About"
+    })
+
